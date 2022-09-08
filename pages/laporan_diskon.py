eabaffcfa92688485a442f19eef19190e5dbcb08
Irvan Sikajudin <41347634+irvansikajudin@users.noreply.github.com>
2022-09-08 22:11:01 +0700

    Create laporan_diskon.py

---
+++ pages/laporan_diskon.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.write('Ini adalah  salah satu report yang pernah saya buat berdasarkan  payment dan pembayaran yang saya buat menggunakan google data studio, tampilan ini di dapatkan dari query SQL dan Server lokal, untuk tampilan terbaik dapat langsung kunjungi di link berikut ini : [Link report](https://datastudio.google.com/embed/reporting/9dcdf68c-e3f8-4b57-a402-530b9298906c/page/AiB1C), dan juga anda dapat melihat jupyter notebook saya sebagai dokumentasi dari projek ini di link berikut ini, buka menggunakan google colab atau jupyter notebook :  [Link .ipynb](https://drive.google.com/file/d/1hNVl9WRlhofc71BO36Upv_GEev-wy3LP/view?usp=sharing)')
+st.markdown("""
+    <iframe width="800" height="500" src="https://datastudio.google.com/embed/reporting/9dcdf68c-e3f8-4b57-a402-530b9298906c/page/AiB1C" frameborder="0" style="border:0" allowfullscreen></iframe>
+    """, unsafe_allow_html=True)
+
+
+
+
