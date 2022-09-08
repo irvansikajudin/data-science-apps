371fe8a5ab7cc29c71c199db0fc4c4d7c5667c8b
Irvan Sikajudin <41347634+irvansikajudin@users.noreply.github.com>
2022-09-09 03:52:19 +0700

    Create deteksi_anomali.py

---
+++ pages/deteksi_anomali.py
@@ -0,0 +1,6 @@
+
+import streamlit as st
+st.write('Anomaly Detection app berada pada link berikut ini : [Link anomaly detection app](https://anomaly-detection-machine-fail.herokuapp.com), hal ini dilakukan karena slug saya terbatas pada 500mb (batas ukuran slug yang diberikan untuk pengguna gratis di heroku.com)')
+
+
+
