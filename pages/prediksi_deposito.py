810b02c7ea8f202d931c05d51ffe37e969eab5d0
Irvan Sikajudin <41347634+irvansikajudin@users.noreply.github.com>
2022-09-09 11:02:32 +0700

    Update prediksi_deposito.py

--- pages/prediksi_deposito.py
+++ pages/prediksi_deposito.py
@@ -16,8 +16,8 @@ Aplikasi ini memprediksi **Kemungkinan Pelanggan Melakukan Deposito**!,
 
 # Loads the Boston House Price Dataset
 # boston = datasets.load_boston()
-# dataset = pd.read_csv('dataset/deposito_clean.csv')
-dataset = pd.read_csv('https://drive.google.com/uc?export=download&id=1xFtcJgieQsq7jgY7F32Jo6vRHR3FQeMv')
+dataset = pd.read_csv('dataset/deposito_clean.csv')
+# dataset = pd.read_csv('https://drive.google.com/uc?export=download&id=1xFtcJgieQsq7jgY7F32Jo6vRHR3FQeMv')
 X = dataset.drop('deposit', axis=1)
 y = dataset['deposit']
 
