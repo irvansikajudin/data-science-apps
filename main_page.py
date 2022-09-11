716e178d8f827877ec983cf5bc286cb92b018a7a
Irvan Sikajudin <41347634+irvansikajudin@users.noreply.github.com>
2022-09-11 13:03:26 +0700

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -2,10 +2,10 @@
 import streamlit as st
 
 st.markdown("# Aplikasi Data Science 🎈")
-st.write("Aplikasi Data Science berbasis web ini dibangun oleh saya, Irvan Sikajudin, sebagai bentuk portfolio.")
-st.write("Saya adalah Alumni Bootcamp Data Science di dibimbing.id, saya memproleh penghargaan 1st Runner Up MVP untuk angkatan saya, Saya juga seorang Sarjana Ekonomi, lulusan fakultas ekonomi dan bisnis universitas tanjungpura, aplikasi ini saya rasa sangat diperlukan karena saya befikir seseorang dengan lulusan sarjana ekonomi seperti saya tidak begitu dapat dipercaya jika berkaitan dengan Data Science khususnya machine learning.")
-st.write("Maka dari itu saya membangun aplikasi ini sebagai pembuktian, yang bertujuan untuk memvalidasi kemampuan saya dibidang data science dan untuk membantu saya mencari pekerjaan pada bidang ini, untuk tampilan terbaik gunakan Desktop/PC/Laptop.")
+st.write("**Aplikasi Data Science berbasis web ini dibangun oleh Irvan Sikajudin, sebagai bentuk portfolio.**")
+st.write("Saya adalah Alumni Bootcamp Data Science di dibimbing.id, saya memproleh penghargaan 1st Runner Up MVP untuk angkatan saya, Saya juga seorang Sarjana Ekonomi, lulusan fakultas ekonomi dan bisnis universitas tanjungpura, Maka dari itu saya membangun aplikasi ini yang bertujuan untuk memvalidasi kemampuan saya dibidang data science dan untuk membantu saya mencari pekerjaan pada bidang ini.")
 st.info("⫷ ⯬Silahkan pilih projek yang ingin anda lihat di sidebar sebelah kiri.  ⫷ ⯬")
-st.error("My Linkedin [link](https://linkedin.com/in/irvansikajudin) || My GitHub [link](https://github.com/irvansikajudin)")
+st.warning("⫷ ⯬Silahkan pilih parameter input yang ingin anda gunakan di sidebar sebelah kiri.  ⫷ ⯬")
+st.error("My Linkedin [link](https://linkedin.com/in/irvansikajudin) || My GitHub [link](https://github.com/irvansikajudin) || irvansika@gmail.com || +6289653989366")
 st.sidebar.info("### Halaman Utama 💯")
 st.sidebar.info("Pada sidebar ini berisikan parameter yang digunakan dalam projek data science yang saya kerjakan, silahkan pilih halaman sesuai projek data science yang ingin anda lihat diatas, maka nantinya parameter akan disesuaikan dengan projek yang sedang dilihat.")
